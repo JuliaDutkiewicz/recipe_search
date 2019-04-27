0f10558b78d100edafd60322df7dd5487764d9e1
Julia Dutkiewicz <dutkiewicz.julia@gmail.com>
2019-04-27 18:41:27 +0200

    GUI improvements and displaying recipes names form API

--- crawler/views.py
+++ crawler/views.py
@@ -1,30 +1,37 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.shortcuts import render
-from django.template import loader
+
+import requests
 
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    meals = []
+    for i in range(5):
+        random_meal = requests.get('https://www.themealdb.com/api/json/v1/1/random.php')
+        if (random_meal.status_code == 200):
+            meals.append(random_meal.json())
+    context = {
+        'recipes' : meals
+    }
+    return render(request, 'index.html', context)
 
 
 def sign_in(request):
-    return render(request, 'working.html')
+    return render(request, 'sign_in.html')
 
 
 def log_in(request):
-    return render(request, 'working.html')
+    return render(request, 'log_in.html')
 
 
 def search(request):
-    return render(request, 'working.html')
+    return render(request, 'search.html')
 
 
 def favorites(request):
-    return render(request, 'working.html')
+    return render(request, 'favorites.html')
