5fce61cbf54dd4bf3e353929f417de7a38e93e45
Julia Dutkiewicz <dutkiewicz.julia@gmail.com>
2019-05-01 17:52:42 +0200

    Hiding API keys

--- crawler/views.py
+++ crawler/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 import requests
+import configuration
 
 
 # Create your views here.
@@ -15,10 +16,10 @@ import requests
 def index(request):
     meals = []
     for i in range(5):
-        random_meal = requests.get('https://www.themealdb.com/api/json/v1/1/random.php')
+        random_meal = requests.get('https://www.themealdb.com/api/json/v2/' + configuration.API_KEY + '/random.php')
         if (random_meal.status_code == 200):
             meals.append(random_meal.json()['meals'][0])
-    first_recipe = requests.get('https://www.themealdb.com/api/json/v1/1/random.php')
+    first_recipe = requests.get('https://www.themealdb.com/api/json/v2/' + configuration.API_KEY + '/random.php')
     if (first_recipe.status_code != 200):
         first_recipe = None
     context = {
@@ -29,16 +30,17 @@ def index(request):
 
 
 def recipe(request, recipe_id):
-    meal = requests.get('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + str(recipe_id))
-    if meal.status_code ==200:
+    meal = requests.get('https://www.themealdb.com/api/json/v2/' + configuration.API_KEY + '/lookup.php?i=' + str(recipe_id))
+    if meal.status_code == 200:
         address = meal.json()["meals"][0]['strYoutube']
         embed_address = address.replace("watch?v=", "embed/")
-        context ={
+        context = {
             "meal": meal.json()["meals"][0],
             "yt_url": embed_address,
         }
         return render(request, 'recipe.html', context)
     return HttpResponse("No such recipe")
+
 
 def sign_in(request):
     return render(request, 'sign_in.html')
